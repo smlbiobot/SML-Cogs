109504c4fcd3ca61b33938ffcde7ad570dc74862
SML BioBot <smlbiobot@gmail.com>
2017-02-05 04:38:32 +0800

    Farmers wip

--- bots/cogs/farmers2.py
+++ bots/cogs/farmers2.py
@@ -34,11 +34,14 @@ from __main__ import send_cmd_help
 
 import os
 import datetime
+import re
 
 
 settings_path = "data/farmers2/settings.json"
 clans = ['alpha', 'bravo', 'charlie', 'delta', 'echo', 'foxtrot', 'golf', 'hotel']
 start_date = datetime.date(2017, 1, 16)
+
+date_re = re.compile('\d{4}\-(0?[1-9]|1[012])\-(0?[1-9]|[12][0-9]|3[01])')
 
 class Farmers2:
     """
@@ -49,6 +52,7 @@ class Farmers2:
 
     def __init__(self, bot):
         self.bot = bot
+        self.file_path = settings_path
         self.settings = dataIO.load_json(settings_path)
 
     @commands.group(pass_context=True, no_pm=True)
@@ -64,26 +68,19 @@ class Farmers2:
 
         server = ctx.message.server
 
-        if server.id not in self.settings:
-            self.settings[server.id] = { 
-                "ServerName": str(server),
-                "ServerID": str(server.id),
-                "ClanChests": { }
-                }
+        self.check_settings(server)
 
         #     for c in clans:
         #         self.settings[server.id]["Trophies"][c] = "0"
 
-        # color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
-        # color = int(color, 16)
+        color = ''.join([choice('0123456789ABCDEF') for x in range(6)])
+        color = int(color, 16)
 
-        # data = discord.Embed(
-        #     color=discord.Color(value=color),
-        #     title="Trophy requirements",
-        #     description="Minimum farmers2 to join our clans. "
-        #                 "Current farmers2 required. "
-        #                 "PB is only used within 12 hours after the clan chest has been completed."
-        #     )
+        data = discord.Embed(
+            color=discord.Color(value=color),
+            title="Clan Chest Farmers",
+            description="Members who have contributed 150+ crowns to clan chest"
+             )
 
         # for clan in clans:
         #     name = '{}{}'.format(clan[0].upper(), clan[1:].lower())
@@ -91,47 +88,120 @@ class Farmers2:
 
         #     data.add_field(name=str(name), value='{:,}'.format(int(value)))
 
-        # if server.icon_url:
-        #     data.set_author(name=server.name, url=server.icon_url)
-        #     data.set_thumbnail(url=server.icon_url)
-        # else:
-        #     data.set_author(name=server.name)
+        if server.icon_url:
+            data.set_author(name=server.name, url=server.icon_url)
+            data.set_thumbnail(url=server.icon_url)
+        else:
+            data.set_author(name=server.name)
 
-        # await self.bot.say(embed=data)
+        await self.bot.say(embed=data)
+
+        self.save_settings()
 
         
                     
     @farmers2.command(name="set", pass_context=True, no_pm=True)
     @checks.mod_or_permissions(mention_everyone=True)
-    async def _set_farmers2(self, ctx, clan:str, req:str):
-        """(MOD) Set the trophy requirements for clans"""
+    async def _set_farmers2(self, ctx, member:discord.Member=None, clan:str=None, crowns:int=None, date:str=None):
+        """
+        Set the trophy requirements for clans.
 
-        # hacking the permission settings
-        # mention_everyone=True for all co-leaders and up
+        Example: !farmers2 set MemberName Alpha 150 2017-02-28
 
-        # server = ctx.message.server
-        # members = server.members
+        """
 
-        # clan = clan.lower()
-
-        # if server.id not in self.settings:
-        #     self.settings[server.id] = { 
-        #         "ServerName": str(server),
-        #         "ServerID": str(server.id),
-        #         "Trophies": { }
-        #         }
-
-        #     for c in clans:
-        #         self.settings[server.id]["Trophies"][c] = "0"
-
-        # if clan not in self.settings[server.id]["Trophies"]:
-        #     await self.bot.say("Clan name is not valid.")
-
+        # Validate arguments before proceeding
+        # try:
+        #     clan = clan.lower()
+        # except:
+        #     await self.bot.say("clan not found")
+        #     await send_cmd_help(ctx)
         # else:
-        #     self.settings[server.id]["Trophies"][clan] = req
-        #     await self.bot.say("Trophy requiremnt for {} updated to {}.".format(clan, req))
+        #     self.bot.say("All good")
 
-        # dataIO.save_json(self.file_path, self.settings)
+        if member is None:
+            await self.bot.say("You must specify a member.")
+            await send_cmd_help(ctx)
+
+        elif clan is None:
+            await self.bot.say("You must enter a clan name.")
+            await send_cmd_help(ctx)
+
+        elif not len(clan) or clan.lower() not in clans:
+            await self.bot.say("You have entered an invalid clan name. "
+                               "Valid clan names incude: {}".format(', '.join(clans)))
+            await send_cmd_help(ctx)
+        elif crowns < 150:
+            await self.bot.say("Members need to contribute more than 150 crowns to be a farmer.")
+            await send_cmd_help(ctx)
+        elif date is None:
+            await self.bot.say("You must entere a date.")
+            await send_cmd_help(ctx)
+        elif not self.check_date(date):
+            await self.bot.say("Date must be entered in YYYY-MM-DD format.")
+        else:
+
+            d = datetime.datetime.strptime(date, "%Y-%m-%d")
+        
+            await self.bot.say("Valid arguments")
+
+            server = ctx.message.server
+            self.check_settings(server)
+
+            # hacking the permission settings
+            # mention_everyone=True for all co-leaders and up
+
+            # server = ctx.message.server
+            # members = server.members
+
+            # clan = clan.lower()
+
+            # if server.id not in self.settings:
+            #     self.settings[server.id] = { 
+            #         "ServerName": str(server),
+            #         "ServerID": str(server.id),
+            #         "Trophies": { }
+            #         }
+
+            #     for c in clans:
+            #         self.settings[server.id]["Trophies"][c] = "0"
+
+            # if clan not in self.settings[server.id]["Trophies"]:
+            #     await self.bot.say("Clan name is not valid.")
+
+            # else:
+            #     self.settings[server.id]["Trophies"][clan] = req
+            #     await self.bot.say("Trophy requiremnt for {} updated to {}.".format(clan, req))
+
+            self.save_settings()
+
+    def check_date(self, date):
+        """
+        Validate date input in YYYY-MM-DD format
+        """
+        try:
+            d = datetime.datetime.strptime(date, "%Y-%m-%d")
+            return True
+        except:
+            return False
+
+        
+    def check_settings(self, server):
+        """
+        Verify that settings are initialized.
+        Initailize if needed.
+        """
+        if server.id not in self.settings:
+            self.settings[server.id] = { 
+                "ServerName": str(server),
+                "ServerID": str(server.id),
+                "ClanChests": { }
+                }
+
+    def save_settings(self):
+        """Saves settings"""
+        dataIO.save_json(self.file_path, self.settings)
+
 
 
 
