457ee18722ce11bb5f137b3b1bea0c03f130cebe
SML BioBot <smlbiobot@gmail.com>
2017-03-14 01:52:33 +0800

    [card] updated file structure in data

--- card/data/scripts/crtexts.py
+++ card/data/scripts/crtexts.py
@@ -1,6 +1,6 @@
 import csv
 import json
-crtexts_path = "data/crtexts.csv"
+crtexts_path = "../csv/crtexts.csv"
 crtexts = {}
 
 with open(crtexts_path, mode='r') as f:
@@ -8,7 +8,7 @@ with open(crtexts_path, mode='r') as f:
     for row in reader:
         crtexts = { row[0]:row[1] for row in reader}
 
-with open("data/crtexts.json", encoding='utf-8', mode="w") as f:
+with open("../crtexts.json", encoding='utf-8', mode="w") as f:
     json.dump(crtexts, f, indent=4,sort_keys=True,
         separators=(',',' : '))
 
