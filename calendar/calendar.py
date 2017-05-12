8cdacd199c090b243a95c958c4746e177f1b18e7
SML BioBot <smlbiobot@gmail.com>
2017-05-13 07:27:17 +0800

    [calendar] removed client secret calls

--- calendar/calendar.py
+++ calendar/calendar.py
@@ -37,9 +37,9 @@ from cogs.utils.dataIO import dataIO
 from __main__ import send_cmd_help
 
 from apiclient import discovery
-from oauth2client import client
-from oauth2client import tools
-from oauth2client.file import Storage
+# from oauth2client import client
+# from oauth2client import tools
+# from oauth2client.file import Storage
 from oauth2client.service_account import ServiceAccountCredentials
 
 from apiclient.discovery import build
@@ -52,34 +52,10 @@ CREDENTIAL_FILENAME = "calendar-credentials.json"
 CREDENTIAL_JSON = os.path.join(PATH, CREDENTIAL_FILENAME)
 
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
-CLIENT_SECRET_JSON = os.path.join(PATH, "client_secret.json")
 SERVICE_KEY_JSON = os.path.join(PATH, "service_key.json")
 APPLICATION_NAME = 'Red Discord Bot Google Calendar API Cog'
 
 RACF_CALENDAR_ID = 'imdea4ui8l6vrpsulmboplsms8@group.calendar.google.com'
-
-def get_credentials():
-    """Get valid user credentials from storage.
-
-    If nothing has been stored, or if the stored credentials are invalid,
-    the OAuth2 flow is completed to obtain the new credentials.
-
-    Returns:
-        Credentials, the obtained credential.
-    """
-    if not os.path.exists(CREDENTIAL_PATH):
-        os.makedirs(CREDENTIAL_PATH)
-
-    store = Storage(CREDENTIAL_JSON)
-    credentials = store.get()
-
-    if not credentials or credentials.invalid:
-        flow = client.flow_from_clientsecrets(CLIENT_SECRET_JSON, SCOPES)
-        flow.user_agent = APPLICATION_NAME
-        flags = None
-        credentials = tools.run_flow(flow, store, flags)
-    return credentials
-
 
 class Practice:
     """RACF Practice."""
@@ -94,30 +70,6 @@ class Practice:
         """Set calendar settings."""
         if ctx.invoked_subcommand is None:
             await send_cmd_help(ctx)
-
-    @setcalendar.command(name="clientsecret", pass_context=True)
-    async def setcalendar_gapisecret(self, ctx):
-        """Set google API client secret.
-
-        This is a json file downloadable from the Google API Console.
-        """
-        await self.bot.say(
-            "Please upload the Google API client_secret.json")
-        answer = await self.bot.wait_for_message(
-            timeout=30.0,
-            author=ctx.message.author)
-        if not len(answer.attachments):
-            await self.bot.say("Cannot find attachments.")
-            return
-        attach = answer.attachments[0]
-        url = attach["url"]
-
-        async with aiohttp.get(url) as cred:
-            with open(CLIENT_SECRET_JSON, "wb") as f:
-                f.write(await cred.read())
-
-        await self.bot.say(
-            "Attachment received: {}".format(CLIENT_SECRET_JSON))
 
     @setcalendar.command(name="servicekey", pass_context=True)
     async def setcalendar_gapisecret(self, ctx):
