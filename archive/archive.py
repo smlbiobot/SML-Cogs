bdf6726557d6278d44c241ae9c3d617b597e6431
SML <smlbiobot@gmail.com>
2017-07-17 23:42:06 +0800

    [archive] added server

--- archive/archive.py
+++ archive/archive.py
@@ -25,6 +25,7 @@ DEALINGS IN THE SOFTWARE.
 """
 
 import os
+from collections import defaultdict
 import discord
 from discord.ext import commands
 
@@ -37,6 +38,11 @@ PATH = os.path.join("data", "archive")
 JSON = os.path.join(PATH, "settings.json")
 
 
+def nested_dict():
+    """Recursively nested defaultdict."""
+    return defaultdict(nested_dict)
+
+
 class Archive:
     """Archive activity.
 
@@ -47,7 +53,8 @@ class Archive:
     def __init__(self, bot):
         """Init."""
         self.bot = bot
-        self.settings = dataIO.load_json(JSON)
+        self.settings = nested_dict()
+        self.settings.update(dataIO.load_json(JSON))
 
     @commands.group(pass_context=True, no_pm=True)
     async def archive(self, ctx):
@@ -64,7 +71,7 @@ class Archive:
 
         await self.bot.say("Channel logged.")
 
-    async def save_channel(self, channel: discord.Channel, count=1000):
+    async def save_channel(self, channel: discord.Channel, count=1000, before=None, after=None, reverse=False):
         """Save channel messages."""
         server = channel.server
         if server.id not in self.settings:
@@ -73,7 +80,7 @@ class Archive:
         channel_messages = []
 
         async for message in self.bot.logs_from(
-                channel, limit=count):
+                channel, limit=count, before=before, after=after, reverse=reverse):
             msg ={
                 'author_id': message.author.id,
                 'content': message.content,
@@ -107,6 +114,88 @@ class Archive:
         server = ctx.message.server
 
         channel_messages = self.settings[server.id][channel.id]
+        for message in channel_messages:
+            author_id = message['author_id']
+            author = server.get_member(author_id)
+            author_mention = author_id
+            if author is not None:
+                author_mention = author.mention
+            content = message['content']
+            timestamp = message['timestamp']
+            message_id = message['id']
+
+            description = '{}: {}'.format(author_mention, content)
+
+            em = discord.Embed(
+                title=channel.name,
+                description=description)
+
+            for reaction in message['reactions']:
+                em.add_field(name=reaction['emoji'], value=reaction['count'])
+
+            em.set_footer(text='{} - ID: {}'.format(timestamp, message_id))
+            await self.bot.say(embed=em)
+
+    @checks.serverowner_or_permissions()
+    @commands.group(pass_context=True, no_pm=True)
+    async def archiveserver(self, ctx):
+        """Archive server."""
+        if ctx.invoked_subcommand is None:
+            await send_cmd_help(ctx)
+
+    @archiveserver.command(name="after", pass_context=True, no_pm=True)
+    async def archiveserver_after(self, ctx, server_name, channel_name, message_id, count=1000):
+        """Archive server after a message"""
+        server = discord.utils.get(self.bot.servers, name=server_name)
+        if server is None:
+            await self.bot.say("Server not found.")
+            return
+        channel = discord.utils.get(server.channels, name=channel_name)
+        if channel is None:
+            await self.bot.say("Channel not found.")
+            return
+        message = self.bot.get_message(channel, message_id)
+
+        await self.log_server_channel(ctx, server, channel, count, after=message)
+        await self.bot.say("Channel logged.")
+
+    async def log_server_channel(
+            self, ctx, server: discord.Server, channel: discord.Channel,
+            count=1000, before=None, after=None, reverse=False):
+        """Save channel messages."""
+        channel_messages = []
+
+        async for message in self.bot.logs_from(
+                channel, limit=count, before=before, after=after, reverse=reverse):
+            msg ={
+                'author_id': message.author.id,
+                'content': message.content,
+                'timestamp': message.timestamp.isoformat(),
+                'id': message.id,
+                'reactions': []
+            }
+            for reaction in message.reactions:
+                r = {
+                    'custom_emoji': reaction.custom_emoji,
+                    'count': reaction.count
+                }
+                if reaction.custom_emoji:
+                    # <:emoji_name:emoji_id>
+                    r['emoji'] = '<:{}:{}>'.format(
+                        reaction.emoji.name,
+                        reaction.emoji.id)
+                else:
+                    r['emoji'] = reaction.emoji
+                msg['reactions'].append(r)
+            channel_messages.append(msg)
+
+        channel_messages = sorted(
+            channel_messages, key=lambda x: x['timestamp'])
+
+        # self.settings[server.id][channel.id] = channel_messages
+        # dataIO.save_json(JSON, self.settings)
+
+        # write out
         for message in channel_messages:
             author_id = message['author_id']
             author = server.get_member(author_id)
