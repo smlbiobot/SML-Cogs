9fa70dae0892f567680524a10c6bcf19d9114d7f
SML <smlbiobot@gmail.com>
2017-08-15 13:10:16 +0800

    [rcs] aded toggleroles subcommand

--- rcs/rcs.py
+++ rcs/rcs.py
@@ -31,6 +31,17 @@ from __main__ import send_cmd_help
 BOTCOMMANDER_ROLES = ['Bot Commander']
 
 
+TOGGLE_ROLES = ["Trusted", "Visitor"]
+
+TOGGLE_PERM = {
+    "Trusted": [
+        "Tournaments"
+    ],
+    "Visitor": [
+    ]
+}
+
+
 class RCS:
     """Reddit Clan System (RCS) utility."""
 
@@ -53,6 +64,55 @@ class RCS:
         roles = list(roles)
         roles.append("Trusted")
         await ctx.invoke(mm.changerole, member, *roles)
+
+    @rcs.command(pass_context=True, no_pm=True)
+    @commands.has_any_role(*TOGGLE_ROLES)
+    async def togglerole(self, ctx, role_name):
+        """Self-toggle role assignments."""
+        author = ctx.message.author
+        server = ctx.message.server
+        # toggleable_roles = [r.lower() for r in TOGGLEABLE_ROLES]
+
+        member_role = discord.utils.get(server.roles, name="Trusted")
+        is_member = member_role in author.roles
+
+        if is_member:
+            toggleable_roles = TOGGLE_PERM["Trusted"]
+        else:
+            toggleable_roles = TOGGLE_PERM["Visitor"]
+
+        toggleable_roles = sorted(toggleable_roles)
+
+        toggleable_roles_lower = [r.lower() for r in toggleable_roles]
+
+        if role_name.lower() in toggleable_roles_lower:
+            role = [
+                r for r in server.roles
+                if r.name.lower() == role_name.lower()]
+
+            if len(role):
+                role = role[0]
+                if role in author.roles:
+                    await self.bot.remove_roles(author, role)
+                    await self.bot.say(
+                        "Removed {} role from {}.".format(
+                            role.name, author.display_name))
+                else:
+                    await self.bot.add_roles(author, role)
+                    await self.bot.say(
+                        "Added {} role for {}.".format(
+                            role_name, author.display_name))
+            else:
+                await self.bot.say(
+                    "{} is not a valid role on this server.".format(role_name))
+        else:
+            out = []
+            out.append(
+                "{} is not a toggleable role for you.".format(role_name))
+            out.append(
+                "Toggleable roles for you: {}.".format(
+                    ", ".join(toggleable_roles)))
+            await self.bot.say("\n".join(out))
 
 
 def setup(bot):
