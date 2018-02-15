d622a9d9bda57a27f4303cb82dc58bf3a5106124
SML <smlbiobot@gmail.com>
2018-02-16 02:34:15 +0800

    [racf_audit] !rs add < to links >

--- racf_audit/racf_audit.py
+++ racf_audit/racf_audit.py
@@ -536,7 +536,7 @@ class RACFAudit:
                     trophies=member_model.get('trophies')
                 ))
                 if pargs.link:
-                    out.append('http://cr-api.com/player/{}'.format(member_model.get('tag')))
+                    out.append('<http://cr-api.com/player/{}>'.format(member_model.get('tag')))
             for page in pagify('\n'.join(out)):
                 await self.bot.say(page)
         else:
