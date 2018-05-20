bc48d6719cc012b3011af52700c11a2d0d9e5498
SML <smlbiobot@gmail.com>
2018-05-21 03:30:31 +0800

    [racf_audit] link add battles, decks, cards

--- racf_audit/racf_audit.py
+++ racf_audit/racf_audit.py
@@ -563,6 +563,9 @@ class RACFAudit:
                 ))
                 if pargs.link:
                     out.append('<http://royaleapi.com/player/{}>'.format(member_model.get('tag')))
+                    out.append('<http://royaleapi.com/player/{}/battles>'.format(member_model.get('tag')))
+                    out.append('<http://royaleapi.com/player/{}/decks>'.format(member_model.get('tag')))
+                    out.append('<http://royaleapi.com/player/{}/cards>'.format(member_model.get('tag')))
             for page in pagify('\n'.join(out)):
                 await self.bot.say(page)
         else:
