dabb1c41833bcc00cb4d3d1018cf6d979552a692
SML <smlbiobot@gmail.com>
2018-07-20 16:46:57 +0800

    [racf] tried and moved back to all embed for cwr

--- racf/racf.py
+++ racf/racf.py
@@ -1492,6 +1492,133 @@ class RACF:
                 "".format(member=member, channel=recruit_channel)
             )
 
+    async def cwready_text(self, tag):
+        url = 'https://royaleapi.com/data/member/war/ready/{}'.format(tag)
+        player_url = "https://royaleapi.com/player/{}".format(tag)
+        conn = aiohttp.TCPConnector(
+            family=socket.AF_INET,
+            verify_ssl=False,
+        )
+        async with aiohttp.ClientSession(connector=conn) as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                elif resp.status == 404:
+                    raise TagNotFound()
+                else:
+                    raise UnknownServerError()
+
+        out = [
+            "{name} #{tag}".format(name=data.get('name'), tag=data.get('tag')),
+            "Clan War Readiness",
+            player_url
+        ]
+
+
+        clan_name = data.get('clan', {}).get('name', '')
+        clan_tag = data.get('clan', {}).get('tag', '')
+        out += [
+            "{} #{}".format(clan_name, clan_tag),
+            "{trophies} / {pb} PB :trophy:".format(
+                trophies=data.get('trophies', 0),
+                pb=data.get('trophies_best', 0)
+            )
+        ]
+        out += [
+            "War Wins: {}".format(data.get('war_day_wins', 0)),
+            "War Cards: {}".format(data.get('clan_cards_collected', 0)),
+            "Challenge Max Wins: {}".format(data.get('challenge_max_wins', 0)),
+            "Challenge Cards Won: {}".format(data.get('challenge_cards_won', 0))
+        ]
+
+        # leagues
+        for league in data.get('leagues'):
+            name = league.get('name')
+            total = league.get('total', 0)
+            percent = league.get('total_percent', 0)
+            levels = league.get('levels', 0)
+            f_name = "{name} League .. {percent:.0%} .. {levels}".format(
+                name=name, total=total, percent=percent, levels=levels
+            )
+
+            out += [f_name]
+
+            cards = league.get('cards', [])
+
+
+            value = ""
+            for card in cards:
+                if card is not None:
+                    value += get_emoji(self.bot, card.get('key', None).replace('-', ''))
+                    if card.get('overlevel'):
+                        value += '+'
+            out += [value]
+
+        return out
+
+    async def cwready_embed2(self, tag):
+        url = 'https://royaleapi.com/data/member/war/ready/{}'.format(tag)
+        player_url = "https://royaleapi.com/player/{}".format(tag)
+        conn = aiohttp.TCPConnector(
+            family=socket.AF_INET,
+            verify_ssl=False,
+        )
+        async with aiohttp.ClientSession(connector=conn) as session:
+            async with session.get(url) as resp:
+                if resp.status == 200:
+                    data = await resp.json()
+                elif resp.status == 404:
+                    raise TagNotFound()
+                else:
+                    raise UnknownServerError()
+
+        em = discord.Embed(
+            title="{name} #{tag}".format(name=data.get('name'), tag=data.get('tag')),
+            description="Clan War Readiness",
+            url=player_url
+        )
+        # em.url = player_url
+        # stats
+        clan_name = data.get('clan', {}).get('name', '')
+        clan_tag = data.get('clan', {}).get('tag', '')
+        em.add_field(name='Clan', value="{} #{}".format(clan_name, clan_tag))
+        em.add_field(
+            name='Trophies',
+            value="{trophies} / {pb} PB :trophy:".format(
+                trophies=data.get('trophies', 0),
+                pb=data.get('trophies_best', 0)
+            ))
+        em.add_field(name='War Wins', value=data.get('war_day_wins', 0))
+        em.add_field(name='War Cards', value=data.get('clan_cards_collected', 0))
+        em.add_field(name='Challenge Max Wins', value=data.get('challenge_max_wins', 0))
+        em.add_field(name='Challenge Cards Won', value=data.get('challenge_cards_won', 0))
+        em.set_footer(text=player_url, icon_url='https://smlbiobot.github.io/img/cr-api/cr-api-logo.png')
+
+        # leagues
+        out = []
+        for league in data.get('leagues'):
+            name = league.get('name')
+            total = league.get('total', 0)
+            percent = league.get('total_percent', 0)
+            levels = league.get('levels', 0)
+            f_name = "{name} League .. {percent:.0%} .. {levels}".format(
+                name=name, total=total, percent=percent, levels=levels
+            )
+
+            out += [f_name]
+
+            cards = league.get('cards', [])
+
+            value = ""
+            for card in cards:
+                if card is not None:
+                    value += get_emoji(self.bot, card.get('key', None).replace('-', ''))
+                    if card.get('overlevel'):
+                        value += '+'
+            out += [value]
+
+        return em, out
+
     async def cwready_embed(self, tag):
         url = 'https://royaleapi.com/data/member/war/ready/{}'.format(tag)
         player_url = "https://royaleapi.com/player/{}".format(tag)
@@ -1578,6 +1705,7 @@ class RACF:
         tag = clean_tag(tag)
 
         try:
+            # em, txt = await self.cwready_embed2(tag)
             em = await self.cwready_embed(tag)
         except TagNotFound:
             await self.bot.say("Invalid tag #{}. Please verify that tag is set correctly.".format(tag))
@@ -1590,6 +1718,9 @@ class RACF:
             await self.bot.say("Server error: {}".format(e))
         else:
             await self.bot.say(embed=em)
+            # for page in pagify("\n".join(txt)):
+            #     await self.bot.say(page)
+
 
     # @checks.mod_or_permissions(manage_roles=True)
     @commands.command(pass_context=True, no_pm=True, aliases=['cwrt'])
@@ -1599,11 +1730,16 @@ class RACF:
 
         try:
             em = await self.cwready_embed(tag)
+            # em, txt = await self.cwready_embed2(tag)
         except Exception as e:
             logger.exception("Unknown exception", e)
             await self.bot.say("Server error: {}".format(e))
         else:
             await self.bot.say(embed=em)
+            # await self.bot.say(embed=em)
+            # for page in pagify("\n".join(txt)):
+            #     if len(page):
+            #         await self.bot.say(page)
 
     # @checks.mod_or_permissions(manage_roles=True)
     @commands.command(pass_context=True, no_pm=True, aliases=['rmre'])
