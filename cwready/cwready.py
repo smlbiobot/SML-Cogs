7e9b64d5accc510dc2f8dbd7041fbf63a8636536
SML <smlbiobot@gmail.com>
2018-07-31 13:41:02 +0800

    [cwready] Add clan war readiness skeleton cog

---
+++ cwready/cwready.py
@@ -0,0 +1,81 @@
+"""
+Clan War Readiness
+"""
+
+# -*- coding: utf-8 -*-
+
+"""
+The MIT License (MIT)
+
+Copyright (c) 2017 SML
+
+Permission is hereby granted, free of charge, to any person obtaining a
+copy of this software and associated documentation files (the "Software"),
+to deal in the Software without restriction, including without limitation
+the rights to use, copy, modify, merge, publish, distribute, sublicense,
+and/or sell copies of the Software, and to permit persons to whom the
+Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+DEALINGS IN THE SOFTWARE.
+"""
+
+import argparse
+import itertools
+import os
+from collections import defaultdict
+from random import choice
+
+import discord
+from cogs.utils import checks
+from cogs.utils.chat_formatting import box
+from cogs.utils.chat_formatting import pagify
+from cogs.utils.dataIO import dataIO
+from discord.ext import commands
+from discord.ext.commands import Context
+
+PATH = os.path.join("data", "cwready")
+JSON = os.path.join(PATH, "settings.json")
+
+
+def nested_dict():
+    """Recursively nested defaultdict."""
+    return defaultdict(nested_dict)
+
+
+class CWReadiness:
+    """Clan War Readinesx"""
+
+    def __init__(self, bot):
+        """Init."""
+        self.bot = bot
+        self.settings = nested_dict()
+        self.settings.update(dataIO.load_json(JSON))
+
+
+
+def check_folder():
+    """Check folder."""
+    os.makedirs(PATH, exist_ok=True)
+
+
+def check_file():
+    """Check files."""
+    if not dataIO.is_valid_json(JSON):
+        dataIO.save_json(JSON, {})
+
+
+def setup(bot):
+    """Setup."""
+    check_folder()
+    check_file()
+    n = CWReadiness(bot)
+    bot.add_cog(n)
