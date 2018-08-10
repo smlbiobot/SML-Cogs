843812ab90a9d7fe19d930882dd3992f9fdb3611
SML <smlbiobot@gmail.com>
2018-08-10 12:12:54 +0800

    [togglerole] self assign roles through emoji

--- togglerole/togglerole.py
+++ togglerole/togglerole.py
@@ -24,14 +24,15 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
-import os
 from collections import defaultdict
 
+import asyncio
 import discord
-from cogs.utils import checks
-from cogs.utils.chat_formatting import pagify, bold
-from cogs.utils.dataIO import dataIO
+import os
 from discord.ext import commands
+
+from cogs.utils import checks
+from cogs.utils.dataIO import dataIO
 
 DATA_PATH = os.path.join("data", "SML-Cogs", "togglerole")
 SETTINGS_JSON = os.path.join(DATA_PATH, "settings.json")
@@ -137,10 +138,12 @@ class ToggleRole:
 
         await self.bot.say(embed=em)
 
-    def check_server_settings(self, server:discord.Server):
+    def check_server_settings(self, server: discord.Server):
         """Check server setttings"""
         if server.id not in self.settings:
             self.settings[server.id] = dict()
+        if "AUTO" not in self.settings[server.id]:
+            self.settings[server.id]["AUTO"] = dict()
         dataIO.save_json(SETTINGS_JSON, self.settings)
 
     def toggleable_roles(self, server, user):
@@ -148,6 +151,9 @@ class ToggleRole:
         o = []
         user_role_names = [r.name for r in user.roles]
         for actor_role, toggle_roles in self.settings[server.id].items():
+            # ignore special AUTO key
+            if actor_role == 'AUTO':
+                continue
             if actor_role == '_everyone':
                 o.extend(toggle_roles.keys())
             if actor_role in user_role_names:
@@ -156,7 +162,7 @@ class ToggleRole:
         o = sorted(o, key=lambda x: x.lower())
         return o
 
-    def toggleable_role_list(self, server, member:discord.Member):
+    def toggleable_role_list(self, server, member: discord.Member):
         """List of toggleable roles for member."""
         toggleable_roles = self.toggleable_roles(server, member)
         if len(toggleable_roles):
@@ -210,17 +216,221 @@ class ToggleRole:
 
     @checks.mod_or_permissions(manage_roles=True)
     @commands.command(pass_context=True, no_pm=True, aliases=["trr"])
-    async def togglerolereact(self, ctx):
+    async def togglerolereact(self, ctx, on_off=True):
         """Create embeds for user to self-toggle via reactions."""
-        channel = ctx.message.channel
         server = ctx.message.server
+        channel = ctx.message.channel
 
         self.check_server_settings(server)
+        self.settings[server.id]["AUTO"] = dict(
+            channel_id=channel.id,
+            on_off=on_off
+        )
+        if on_off:
+            await self.post_togglerole_embeds(server, channel)
+
+    def role_embed(self, server, t_role, actor_roles):
+        desc = ", ".join(actor_roles)
+        if len(desc) == 0:
+            return None
+
+        em = discord.Embed(
+            title=t_role,
+            description=desc
+        )
+
+        # list members
+        r_role_o = discord.utils.get(server.roles, name=t_role)
+        m_with_role = []
+        for member in server.members:
+            if r_role_o in member.roles:
+                m_with_role.append(member)
+
+        name = "Members"
+        value = ", ".join([m.name for m in m_with_role])
+        if len(value) > 1000:
+            value = value[:1000] + "…"
+        if len(m_with_role) == 0:
+            value = 'None'
+        em.add_field(name=name, value=value)
+        return em
+
+    async def post_togglerole_embeds(self, server, channel):
+        """Post embeds for user to self-toggle via reactions."""
+        self.check_server_settings(server)
+
+        # create list of toggleable roles
+        toggleables = dict()
+
+        for actor_role, toggle_roles in self.settings[server.id].items():
+            # ignore special AUTO key
+            if actor_role == 'AUTO':
+                continue
+
+            for t_role in toggle_roles:
+                if t_role not in toggleables.keys():
+                    toggleables[t_role] = []
+                toggleables[t_role].append(actor_role)
+
+        message = None
+        for t_role, actor_roles in toggleables.items():
+            em = self.role_embed(server, t_role, actor_roles)
+            if em is None:
+                continue
+
+            msg = await self.bot.send_message(channel, embed=em)
+
+            await self.bot.add_reaction(msg, "✅")
+            await self.bot.add_reaction(msg, "❌")
+
+            # save first message
+            if message is None:
+                message = msg
 
         # delete channel messages
         if channel is not None:
-            await self.bot.purge_from(channel, limit=10, before=ctx.message)
+            await self.bot.purge_from(channel, limit=100, before=message)
 
+    async def post_togglerole_task(self):
+        """Auto post tasks."""
+        while self == self.bot.get_cog("ToggleRole"):
+            try:
+                for server_id, settings in self.settings.items():
+                    server = self.bot.get_server(server_id)
+                    if server is None:
+                        continue
+                    self.check_server_settings(server)
+                    if self.settings[server.id].get("AUTO", {}).get("on_off", False):
+                        channel_id = self.settings.get(server.id, {}).get("AUTO", {}).get("channel_id")
+                        if channel_id is None:
+                            continue
+                        channel = server.get_channel(channel_id)
+                        if channel is None:
+                            continue
+                        await self.post_togglerole_embeds(server, channel)
+            except Exception:
+                pass
+            finally:
+                await asyncio.sleep(57)
+
+    async def on_reaction_add(self, reaction: discord.Reaction, user):
+        await self.on_reaction(reaction, user)
+
+    # async def on_reaction_remove(self, reaction: discord.Reaction, user):
+    #     await self.on_reaction(reaction, user, remove=True)
+
+    async def on_reaction(self, reaction: discord.Reaction, user):
+        message = reaction.message
+        channel = message.channel
+        server = message.server
+        if user == self.bot.user:
+            return
+
+        # create list of tasks
+        tasks = []
+
+        # tasks after
+        update_tasks = []
+
+        for server_id in self.settings.keys():
+            auto = self.settings.get(server_id, {}).get("AUTO", {})
+            # auto must be on
+            if not auto.get('on_off'):
+                continue
+            # channel id must match
+            if auto.get("channel_id") != channel.id:
+                continue
+                # must not be bot
+            if user == self.bot.user:
+                continue
+
+            add = False
+            remove = False
+
+            if reaction.emoji == '✅':
+                add = True
+            elif reaction.emoji == '❌':
+                remove = True
+            else:
+                await self.bot.remove_reaction(message, reaction.emoji, user)
+
+            # must be on embed
+            if len(message.embeds) == 0:
+                continue
+            em = message.embeds[0]
+            role_name = em.get('title')
+            allowed_roles = em.get('description', []).split(", ")
+
+            # must be valid
+            if role_name is None:
+                continue
+            if len(allowed_roles) == 0:
+                continue
+
+            # check user has allowed_roles
+            valid = False
+            for u_role in user.roles:
+                if u_role.name in allowed_roles:
+                    valid = True
+                    break
+            if not valid:
+                continue
+
+            # check if user has role
+            user_has_role = False
+            for u_role in user.roles:
+                if u_role.name == role_name:
+                    user_has_role = True
+
+            update_messages = False
+
+            if add:
+                if user_has_role:
+                    txt = "User already has the role."
+                    tasks.append(self.delay_message(channel, txt))
+                else:
+                    tasks.append(self.add_role(server, user, role_name, channel=channel))
+                    update_messages = True
+            elif remove:
+                if not user_has_role:
+                    txt = "User does not have the role."
+                    tasks.append(self.delay_message(channel, txt))
+                else:
+                    tasks.append(self.remove_role(server, user, role_name, channel=channel))
+                    update_messages = True
+
+            # remove reaction
+            tasks.append(self.bot.remove_reaction(message, reaction.emoji, user))
+
+            # update messages
+            if update_messages:
+                update_tasks.append(self.post_togglerole_embeds(server, channel))
+
+        if len(tasks):
+            await asyncio.gather(*tasks)
+
+        if len(update_tasks):
+            await asyncio.gather(*update_tasks)
+
+    async def delay_message(self, channel, txt, wait=5):
+        msg = await self.bot.send_message(channel, txt)
+        await asyncio.sleep(wait)
+        await self.bot.delete_message(msg)
+
+    async def add_role(self, server, user, role_name, channel=None):
+        role = discord.utils.get(server.roles, name=role_name)
+        await self.bot.add_roles(user, role)
+        if channel is not None:
+            txt = "Added {} to {}".format(role, user)
+            await self.delay_message(channel, txt)
+
+
+    async def remove_role(self, server, user, role_name, channel=None):
+        role = discord.utils.get(server.roles, name=role_name)
+        await self.bot.remove_roles(user, role)
+        if channel is not None:
+            txt = "Removed {} from {}".format(role, user)
+            await self.delay_message(channel, txt)
 
 
 def check_folder():
@@ -240,3 +450,4 @@ def setup(bot):
     check_file()
     n = ToggleRole(bot)
     bot.add_cog(n)
+    bot.loop.create_task(n.post_togglerole_task())
