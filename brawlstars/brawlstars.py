b6aa2c44b547ba4ff41e7744e4144c17e00146ee
SML <smlbiobot@gmail.com>
2018-11-04 16:59:09 +0800

    [brawlstars] Add api fetch

--- brawlstars/brawlstars.py
+++ brawlstars/brawlstars.py
@@ -24,6 +24,7 @@ DEALINGS IN THE SOFTWARE.
 
 import argparse
 import itertools
+import socket
 import os
 from collections import defaultdict
 from random import choice
@@ -34,15 +35,58 @@ from cogs.utils.chat_formatting import box
 from cogs.utils.chat_formatting import pagify
 from cogs.utils.dataIO import dataIO
 from discord.ext import commands
-from discord.ext.commands import Context
+import aiohttp
 
 PATH = os.path.join("data", "brawlstars")
 JSON = os.path.join(PATH, "settings.json")
+
+from box import Box
 
 
 def nested_dict():
     """Recursively nested defaultdict."""
     return defaultdict(nested_dict)
+
+def clean_tag(tag):
+    """Clean supercell tag"""
+    t = tag
+    t = t.upper()
+    t = t.replace('O', '0')
+    t = t.replace('B', '8')
+    t = t.replace('#', '')
+    return t
+
+
+
+class BSPlayer(Box):
+    """Player model"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+class BSClan(Box):
+    """Player model"""
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+async def api_fetch(url=None, auth=None):
+    """Fetch from BS API"""
+    conn = aiohttp.TCPConnector(
+        family=socket.AF_INET,
+        verify_ssl=False,
+    )
+    async with aiohttp.ClientSession(connector=conn) as session:
+        async with session.get(url, headers=dict(Authorization=auth)) as resp:
+            data = await resp.json()
+    return data
+
+
+async def api_fetch_player(tag=None, auth=None):
+    """Fetch player"""
+    url = 'https://brawlapi.cf/api/players/{}'.format(clean_tag(tag))
+    data = await api_fetch(url=url, auth=auth)
+    return BSPlayer(data)
+
 
 
 class BrawlStars:
@@ -80,9 +124,40 @@ class BrawlStars:
     async def bsset_auth(self, ctx, token):
         """Authorization (token)."""
         self.settings['brawlapi_token'] = token
-        if self._save_settings:
+        if self._save_settings():
             await self.bot.say("Authorization (token) updated.")
         await self.bot.delete_message(ctx.message)
+
+    @commands.group(pass_context=True, no_pm=True)
+    async def bs(self, ctx):
+        """Brawl Stars."""
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @bs.command(name="settag", alias=['st'], pass_context=True)
+    async def bs_settag(self, ctx, tag):
+        """Assign tag to self."""
+        tag = clean_tag(tag)
+        server = ctx.message.server
+        author = ctx.message.author
+        self.settings[server.id][author.id] = tag
+        if self._save_settings():
+            await self.bot.say("Tag saved.")
+
+
+    @bs.command(name="profile", aliases=['p'], pass_context=True)
+    async def bs_profile(self, ctx, tag=None):
+        """BS Profile."""
+        server = ctx.message.server
+        author = ctx.message.author
+        if tag is None:
+            tag = self.settings.get(server.id, {}).get(author.id)
+            if tag is None:
+                await self.bot.say("Can’t find tag associated with user.")
+
+        await self.bot.say(tag)
+        player = await api_fetch_player(tag=tag, auth=self.settings.get('brawlapi_token'))
+        await self.bot.say(player.name)
 
 
 def check_folder():
