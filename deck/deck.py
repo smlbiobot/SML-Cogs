e856392dc97883190fc1140017a3b0a63c28200b
smlbiobot <smlbiobot@gmail.com>
2019-05-17 21:15:25 +0800

    Add goblin cage

--- deck/deck.py
+++ deck/deck.py
@@ -930,6 +930,7 @@ class Deck:
                 except discord.DiscordException:
                     pass
 
+
     async def post_deck(self, channel=None, title=None, description=None, timestamp=None, card_keys=None,
                         deck_name=None, deck_author=None, color=None):
         """Post a deck to destination channel.
