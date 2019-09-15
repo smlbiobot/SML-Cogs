c1ee80ba288506a4eb87b031f325f393c60909c9
smlbiobot <smlbiobot@gmail.com>
2019-09-15 12:22:23 +0800

    [rushwars] Update copyright

--- rushwars/rushwars.py
+++ rushwars/rushwars.py
@@ -1,7 +1,7 @@
 """
 The MIT License (MIT)
 
-Copyright (c) 2018 SML
+Copyright (c) 2019 SML <sml@royaleapi.com>
 
 Permission is hereby granted, free of charge, to any person obtaining a
 copy of this software and associated documentation files (the "Software"),
