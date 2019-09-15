6169f3321776396d262c23b6c4aa13df42f2b8aa
smlbiobot <smlbiobot@gmail.com>
2019-09-15 11:29:55 +0800

    [rushwars] skeleton

--- rushwars/rushwars.py
+++ rushwars/rushwars.py
@@ -22,19 +22,29 @@ FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
 
+import asyncio
+import json
 import os
+import socket
 from collections import defaultdict
 
+import aiofiles
+import aiohttp
 import discord
+import yaml
+from box import Box
+from cogs.utils import checks
 from cogs.utils.dataIO import dataIO
 from discord.ext import commands
+from discord.ext.commands import MemberConverter
+from discord.ext.commands.errors import BadArgument
 
 PATH = os.path.join("data", "rushwars")
 JSON = os.path.join(PATH, "settings.json")
-BAND_CONFIG_YML = os.path.join(PATH, "club.config.yml")
+TEAM_CONFIG_YML = os.path.join(PATH, "team.config.yml")
 CACHE_PATH = os.path.join(PATH, "cache")
 CACHE_PLAYER_PATH = os.path.join(CACHE_PATH, "player")
-CACHE_CLUB_PATH = os.path.join(CACHE_PATH, "club")
+CACHE_TEAM_PATH = os.path.join(CACHE_PATH, "team")
 
 MANAGE_ROLE_ROLES = ['Bot Commander']
 
@@ -54,7 +64,107 @@ def clean_tag(tag):
     return t
 
 
-class BrawlStars:
+class RushWarsAPIError(Exception):
+    pass
+
+
+class APIRequestError(RushWarsAPIError):
+    pass
+
+
+class APITimeoutError(RushWarsAPIError):
+    pass
+
+
+class APIServerError(RushWarsAPIError):
+    pass
+
+
+class RWPlayer(Box):
+    """Player model"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class RWTeam(Box):
+    """Team model"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+
+class RushWarsAPI:
+    """Rush Wars API"""
+
+    def __init__(self, auth):
+        self._auth = auth
+
+    def set_auth(self, auth):
+        self._auth = auth
+
+    async def fetch(self, url=None, auth=None):
+        """Fetch from API"""
+        conn = aiohttp.TCPConnector(
+            family=socket.AF_INET,
+            verify_ssl=False,
+        )
+        if auth is None:
+            auth = self._auth
+        try:
+            async with aiohttp.ClientSession(connector=conn) as session:
+                async with session.get(url, headers=dict(Authorization=auth), timeout=10) as resp:
+
+                    if str(resp.status).startswith('4'):
+                        raise APIRequestError()
+
+                    if str(resp.status).startswith('5'):
+                        raise APIServerError()
+
+                    data = await resp.json()
+        except asyncio.TimeoutError:
+            raise APITimeoutError()
+
+        return data
+
+    async def fetch_player(self, tag=None, auth=None, **kwargs):
+        """Fetch player"""
+        url = 'https://api.brawlapi.cf/v1/player?tag={}'.format(clean_tag(tag))
+        fn = os.path.join(CACHE_PLAYER_PATH, "{}.json".format(tag))
+        try:
+            data = await self.fetch(url=url, auth=auth)
+        except APIServerError:
+            if os.path.exists(fn):
+                async with aiofiles.open(fn, mode='r') as f:
+                    data = json.load(await f.read())
+            else:
+                raise
+        else:
+            async with aiofiles.open(fn, mode='w') as f:
+                json.dump(data, f)
+
+        return RWPlayer(data)
+
+    async def fetch_team(self, tag=None, auth=None, **kwargs):
+        """Fetch player"""
+        url = 'https://api.brawlapi.cf/v1/club?tag={}'.format(clean_tag(tag))
+        fn = os.path.join(CACHE_TEAM_PATH, "{}.json".format(tag))
+        try:
+            data = await self.fetch(url=url, auth=auth)
+        except APIServerError:
+            if os.path.exists(fn):
+                async with aiofiles.open(fn, mode='r') as f:
+                    data = json.load(await f.read())
+            else:
+                raise
+        else:
+            async with aiofiles.open(fn, mode='w') as f:
+                json.dump(data, f)
+
+        return RWTeam(data)
+
+
+class RushWars:
     """Brawl Stars API"""
 
     def __init__(self, bot):
@@ -68,18 +178,81 @@ class BrawlStars:
         dataIO.save_json(JSON, self.settings)
         return True
 
+    async def _get_team_config(self, force_update=False):
+        if force_update or self._club_config is None:
+            async with aiofiles.open(TEAM_CONFIG_YML) as f:
+                contents = await f.read()
+                self._club_config = yaml.load(contents, Loader=yaml.FullLoader)
+        return self._club_config
+
+    async def _get_server_config(self, server_id=None):
+        cfg = await self._get_team_config()
+        for server in cfg.get('servers', []):
+            if str(server.get('id')) == str(server_id):
+                return server
+        return None
+
+    async def send_error_message(self, ctx):
+        channel = ctx.message.channel
+        await self.bot.send_message(channel, "RushWarsAPI Error. Please try again later…")
+
     def get_emoji(self, name):
         for emoji in self.bot.get_all_emojis():
             if emoji.name == str(name):
                 return '<:{}:{}>'.format(emoji.name, emoji.id)
         return ''
 
-    async def _get_server_config(self, server_id=None):
-        cfg = await self._get_club_config()
-        for server in cfg.get('servers', []):
-            if str(server.get('id')) == str(server_id):
-                return server
-        return None
+    @commands.group(pass_context=True, no_pm=True)
+    @checks.serverowner_or_permissions()
+    async def rwset(self, ctx):
+        """Set Rush Wars API settings.
+
+        Require https://rushstats.com/api
+        """
+        if ctx.invoked_subcommand is None:
+            await self.bot.send_cmd_help(ctx)
+
+    @rwset.command(name="init", pass_context=True)
+    async def _rwset_init(self, ctx):
+        """Init BS Band settings."""
+        server = ctx.message.server
+        self.settings[server.id] = {}
+        if self._save_settings:
+            await self.bot.say("Server settings initialized.")
+
+    @rwset.command(name="auth", pass_context=True)
+    async def _rwset_auth(self, ctx, token):
+        """Authorization (token)."""
+        self.settings['api_token'] = token
+        if self._save_settings():
+            await self.bot.say("Authorization (token) updated.")
+        await self.bot.delete_message(ctx.message)
+
+    @rwset.command(name="config", pass_context=True)
+    async def _rwset_config(self, ctx):
+        """Team config"""
+        if len(ctx.message.attachments) == 0:
+            await self.bot.say(
+                "Please attach config yaml with this command. "
+                "See config.example.yml for how to format it."
+            )
+            return
+
+        attach = ctx.message.attachments[0]
+        url = attach["url"]
+
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                with open(TEAM_CONFIG_YML, "wb") as f:
+                    f.write(await resp.read())
+
+        await self.bot.say(
+            "Attachment received and saved as {}".format(TEAM_CONFIG_YML))
+
+        self.settings['config'] = TEAM_CONFIG_YML
+        dataIO.save_json(JSON, self.settings)
+
+        await self.bot.delete_message(ctx.message)
 
     @commands.group(pass_context=True, no_pm=True)
     async def rw(self, ctx):
@@ -87,9 +260,32 @@ class BrawlStars:
         if ctx.invoked_subcommand is None:
             await self.bot.send_cmd_help(ctx)
 
+    @rw.command(name="player", aliases=['p', 'profile'], pass_context=True)
+    async def rw_player(self, ctx, player):
+        """Player profile.
+
+        player can be a player tag or a Discord member.
+        """
+        server = ctx.message.server
+        member = None
+        tag = None
+        try:
+            cvt = MemberConverter(ctx, player)
+            member = cvt.convert()
+        except BadArgument:
+            # cannot convert to member. Assume argument is a tag
+            tag = clean_tag(player)
+        else:
+            tag = self.settings.get(server.id, {}).get(member.id)
+            if tag is None:
+                await self.bot.say("Can’t find tag associated with user.")
+                return
+
+        await self.bot.say("{member} {tag}".format(member=member, tag=tag))
+
     @rw.command(name="verify", aliases=['v'], pass_context=True)
     @commands.has_any_role(*MANAGE_ROLE_ROLES)
-    async def bs_verify(self, ctx, member: discord.Member, tag=None):
+    async def rw_verify(self, ctx, member: discord.Member, tag=None):
         tag = clean_tag(tag)
         ctx_server = ctx.message.server
 
@@ -123,7 +319,7 @@ def check_folder():
     os.makedirs(PATH, exist_ok=True)
     os.makedirs(CACHE_PATH, exist_ok=True)
     os.makedirs(CACHE_PLAYER_PATH, exist_ok=True)
-    os.makedirs(CACHE_CLUB_PATH, exist_ok=True)
+    os.makedirs(CACHE_TEAM_PATH, exist_ok=True)
 
 
 def check_file():
@@ -136,5 +332,5 @@ def setup(bot):
     """Setup."""
     check_folder()
     check_file()
-    n = BrawlStars(bot)
+    n = RushWars(bot)
     bot.add_cog(n)
