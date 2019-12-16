10237397247c0361870ff209966a35f160318c73
smlbiobot <smlbiobot@gmail.com>
2019-12-17 01:41:40 +0800

    [message_quote] Support multiple message quote

--- message_quote/message_quote.py
+++ message_quote/message_quote.py
@@ -43,16 +43,30 @@ class MessageQuote:
         self.bot = bot
 
     @commands.command(name="mq", pass_context=True)
-    async def message_quote(self, ctx, message_id, channel: discord.Channel = None):
-        """Quote a message by ID
+    async def message_quote(self, ctx, *, args):
+        """Quote message(s) by ID
 
+        !mq 582794656560054272
         !mq 582794656560054272 #family-chat
+        !mq 582794656560054272 582794656560054273 #family-chat
         If channel is omitted, use current channel
         """
+        args = args.split(" ")
+        last_arg = args[-1]
+        from discord.ext.commands.converter import ChannelConverter
+        from discord.ext.commands.errors import BadArgument
+        try:
+            channel = ChannelConverter(ctx, last_arg).convert()
+        except BadArgument:
+            channel = None
+        else:
+            args = args[:-1]
         if channel is None:
             channel = ctx.message.channel
 
-        await self._show_message(ctx, channel, message_id)
+        message_ids = args
+
+        await self._show_message(ctx, channel, message_ids)
 
     @checks.mod_or_permissions()
     @commands.command(name="mqs", pass_context=True)
@@ -71,55 +85,61 @@ class MessageQuote:
 
         await self._show_message(ctx, channel, message_id)
 
-    async def _show_message(self, ctx, channel: discord.Channel, message_id):
-        try:
-            msg = await self.bot.get_message(channel, message_id)
-        except discord.NotFound:
-            await self.bot.say("Message not found.")
-            return
-        except discord.Forbidden:
-            await self.bot.say("I do not have permissions to fetch the message")
-            return
-        except discord.HTTPException:
-            await self.bot.say("Retrieving message failed")
-            return
+    async def _show_message(self, ctx, channel: discord.Channel, message_ids):
+        messages = []
+        for message_id in message_ids:
+            try:
+                msg = await self.bot.get_message(channel, message_id)
+            except discord.NotFound:
+                await self.bot.say("Message not found.")
+                return
+            except discord.Forbidden:
+                await self.bot.say("I do not have permissions to fetch the message")
+                return
+            except discord.HTTPException:
+                await self.bot.say("Retrieving message failed")
+                return
 
-        if not msg:
-            return
+            if not msg:
+                continue
 
-        ts = msg.timestamp
+            messages.append(msg)
 
-        out = [
-            msg.content or '',
-            "— {}, {}".format(
-                msg.author.mention,
-                ts.isoformat(sep=" ")
+        for msg in messages:
+
+            ts = msg.timestamp
+
+            out = [
+                msg.content or '',
+                "— {}, {}".format(
+                    msg.author.mention,
+                    ts.isoformat(sep=" ")
+                )
+            ]
+
+            link = 'https://discordapp.com/channels/{server_id}/{channel_id}/{message_id}'.format(
+                server_id=msg.server.id,
+                channel_id=msg.channel.id,
+                message_id=msg.id
             )
-        ]
 
-        link = 'https://discordapp.com/channels/{server_id}/{channel_id}/{message_id}'.format(
-            server_id=msg.server.id,
-            channel_id=msg.channel.id,
-            message_id=msg.id
-        )
+            em = discord.Embed(
+                title="Quote",
+                description="\n".join(out),
+                url=link
+            )
 
-        em = discord.Embed(
-            title="Quote",
-            description="\n".join(out),
-            url=link
-        )
+            if msg.attachments:
+                url = msg.attachments[0].get('url')
+                if url:
+                    em.set_image(url=url)
 
-        if msg.attachments:
-            url = msg.attachments[0].get('url')
-            if url:
-                em.set_image(url=url)
+            em.set_footer(
+                text=msg.server.name,
+                icon_url=msg.server.icon_url
+            )
 
-        em.set_footer(
-            text=msg.server.name,
-            icon_url=msg.server.icon_url
-        )
-
-        await self.bot.send_message(ctx.message.channel, embed=em)
+            await self.bot.send_message(ctx.message.channel, embed=em)
 
 
 def check_folder():
