a934819c417ab3de8a46413e89962236b602f84e
SML <smlbiobot@gmail.com>
2021-04-06 11:15:21 +0800

    [deck] update cards constants

--- deck/deck.py
+++ deck/deck.py
@@ -23,7 +23,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
 FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
 DEALINGS IN THE SOFTWARE.
 """
-# force update skeleton dragons 2
+# force update cards json
 import datetime
 import datetime as dt
 import io
